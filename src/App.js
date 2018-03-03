798d87e8204147541fa48bdd8a8dee218f914b36
tao <v.kostiuk@internetdevels.com>
2018-03-03 11:02:31 +0200

    [CH-66] - Base frame of codehub map

--- src/App.js
+++ src/App.js
@@ -5,14 +5,24 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="ch">
+        <div className="ch-menu">
+          <section className="ch-city-logo">
+             <a href="#" className="logo">Logo</a>
+          </section>
+          <nav className="ch-top-menu">
+            <a href="#" className="home">Home</a>
+            <a href="#" className="home">Layers</a>
+          </nav>
+          <nav className="ch-bottom-menu">
+            <a href="#" className="home">Help</a>
+            <a href="#" className="home">Menu</a>
+            <a href="#" className="home">User</a>
+          </nav>
+        </div>
+        <div className="ch-content">
+           Some content.
+        </div>
       </div>
     );
   }
