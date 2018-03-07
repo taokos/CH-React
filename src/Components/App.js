f38a5a17ab1da65d5f9af4fa137fb8aaf0dd39a1
tao <v.kostiuk@internetdevels.com>
2018-03-07 19:03:05 +0200

    [CH-55] - Map Layers

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {BrowserRouter, Route} from "react-router-dom"
 
+
 import '../css/App.css';
 
 import LMap from './Map';
--- src/Components/Map.js
+++ src/Components/Map.js
@@ -1,59 +1,43 @@
 import React, { Component } from 'react';
-import LLayers from './Layers'
-import {
-  Map,
-  TileLayer,
-} from 'react-leaflet'
 import '../css/leaflet.css';
+import L from 'leaflet'
+import {GroupedLayers} from 'leaflet-groupedlayercontrol'
+import groupedOverlays from './Layers.js';
 
 
-const center = [25.64837124674059, -80.712685]
+const center = [25.64837124674059, -80.712685];
 
-const DEFAULT_VIEWPORT = {
-  center: center,
-  zoom: 10,
-  LayersControl: true,
-  attributionControl: true,
-  closePopupOnClick: true,
-  doubleClickZoom: true,
-  dragging: true,
-  fadeAnimation: true,
-  maxZoom: 18,
-  minZoom: 0,
-  scrollWheelZoom: true,
-  touchZoom: true,
-  trackResize: true,
-  zoomAnimation: true,
-  zoomControl: false,
-};
+class LMap extends React.Component {
+  componentDidMount() {
+    this.map();
+  }
+  
+  map() {
+    var map = L.map('map').setView(center, 10);
 
+    const streeAttr = '&amp;copy <a href=\'https://www.mapbox.com/about/maps/\'>Mapbox</a> © <a href=\'http://www.openstreetmap.org/copyright\'>OpenStreetMap</a> contributors',
+      streeUrl = 'https://{s}.tiles.mapbox.com/v3/inhabitmiami.l269hdof/{z}/{x}/{y}.png',
+      satelliteAttr = '<a href="https://www.mapbox.com/about/maps/" target="_blank">© Mapbox</a> <a href="http://www.openstreetmap.org/about/" target="_blank">© OpenStreetMap</a> <a class="mapbox-improve-map" href="https://www.mapbox.com/map-feedback/" target="_blank">Improve this map</a> <a href="https://www.digitalglobe.com/" target="_blank">© DigitalGlobe</a>',
+      satelliteUrl = 'https://api.mapbox.com/v4/mapbox.satellite/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoiaW5oYWJpdG1pYW1pIiwiYSI6IlVqbDFmYW8ifQ._WCxjKTHYFmq6bdIUeLwYA';
+  
+    const street   = L.tileLayer(streeUrl, {attribution: streeAttr, checked: true}),
+      satellite  = L.tileLayer(satelliteUrl, {attribution: satelliteAttr});
+  
 
-class LMap extends Component {
-
-  state = {
-    viewport: DEFAULT_VIEWPORT,
-  };
-
-  onClickReset = () => {
-    this.setState({ viewport: DEFAULT_VIEWPORT })
-  };
-  onViewportChanged = viewport => {
-    this.setState({ viewport })
-  };
+    const baseLayers = {
+      "Streets": street,
+      "Satellite": satellite
+    };
+    
+    L.control.groupedLayers(baseLayers, groupedOverlays).addTo(map);
+    
+    L.tileLayer(streeUrl, {
+      attribution: streeAttr
+    }).addTo(map);
+  }
+  
   render() {
-    return (
-      <div className="ch-map">
-        <Map
-          onClick={this.onClickReset}
-          onViewportChanged={this.onViewportChanged}
-          viewport={this.state.viewport}>
-          <TileLayer
-            url="https://{s}.tiles.mapbox.com/v3/inhabitmiami.l269hdof/{z}/{x}/{y}.png"
-          />
-          <LLayers />
-        </Map>
-      </div>
-    )
+    return <div id="map" className="ch-map">xx</div>
   }
 }
 
