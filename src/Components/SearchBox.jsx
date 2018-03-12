151d53280ff25afe0becf115d50196464ab31947
Sergiy Veremchuk <s.veremchuk@internetdevels.com>
2018-03-12 18:08:33 +0200

    [CH-59] Search page

--- src/Components/SearchBox.jsx
+++ src/Components/SearchBox.jsx
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class SearchBox extends Component {
   render() {
     return (
-      <div>
+      <div className="search-box">
         <form method="GET" action={'/us/' + this.props.match.params.p1 + '/' + this.props.match.params.p2 + '/search'}>
           <input name="search" type="search" placeholder="Search" ref="search"/>
         </form>
--- src/Components/SearchResults.jsx
+++ src/Components/SearchResults.jsx
@@ -5,32 +5,98 @@ const API = '/api/v1/codehub/0';
 class SearchResults extends Component {
   constructor(props) {
     super(props);
-    
+
     this.state = {
       matchedResults: [],
+      pieces: []
     };
   }
 
   componentDidMount() {
-    fetch(API + this.props.history.location.search + '&alias=/us/' + this.props.match.params.p1+ '/'+ this.props.match.params.p2 + '&_format=json')
+    const requestUrl = API
+      + this.props.history.location.search
+      + '&alias=/us/'
+      + this.props.match.params.p1
+      + '/'
+      + this.props.match.params.p2
+      + '&_format=json&l=10';
+    fetch(requestUrl)
       .then(results => results.json())
       .then(data => this.setState({ matchedResults: data}))
+      .then(data => this.getParents())
   }
-  
+
+  getParents() {
+    let breadcrumbs = [];
+    let pieces = [];
+    this.state.matchedResults.items.map((result) =>
+    {pieces = result.doc.path.split('/').filter(item => item !== '' && item !== result.doc.id);
+    breadcrumbs += ',' + pieces;
+    });
+
+    const request = API
+      + this.props.history.location.search
+      + '&alias=/us/'
+      + this.props.match.params.p1
+      + '/'
+      + this.props.match.params.p2
+      + '&_format=json&ids='
+      + breadcrumbs;
+
+    fetch(request)
+      .then(results => results.json())
+      .then(data => this.setState({ pieces: data}));
+  }
+
   prepareResults(data) {
-    return data.map((result, i) =>
-      <div data-value={result.doc.path} key={i}>{result.highlight.text}</div>
+    if (typeof data.items === 'undefined') {
+      data.items = [];
+    }
+    if (this.state.pieces.length < 1) {
+      return '';
+    }
+
+    for (let i in data.items) {
+      let path = data.items[i].doc.path.split('/').filter(item => item !== '');
+      for (let key in path) {
+        if (typeof this.state.pieces[path[key]] !== 'undefined') {
+          path[key] = (<a key={'link-'+ key} href={'#' + path[key]}>{this.state.pieces[path[key]].text}</a>);
+        }
+        else {
+          delete path[key];
+        }
+      }
+
+      // Hasn't parents.
+      if (path.length <= 1) {
+        data.items[i].doc.breadcrumbs = (<a key={'link-'+ 1} href={'#' + data.items[i].doc.id}>{data.items[i].doc.text}</a>);
+      }
+      // Has parents.
+      else {
+        data.items[i].doc.breadcrumbs = path.reverse();
+      }
+    }
+
+    return data.items.map((result, i) =>
+      <div key={'result-'+i}>
+        <div className="breadcrumbs">{result.doc.breadcrumbs}</div>
+        <div className="title">{result.doc.breadcrumbs[1]}</div>
+        <div className="matches" data-value={result.doc.path} key={i} dangerouslySetInnerHTML={{__html: result.highlight.text}} />
+      </div>
     );
   }
 
   render() {
     const { matchedResults } = this.state;
+    const urlParams = new URLSearchParams(this.props.history.location.search);
+
     return (
       <div className="ch-results">
+        <div className="ch-searching">Search Results for “{urlParams.get('search')}“</div>
+        <div className="ch-total-results">That search returned {matchedResults.total} results.</div>
         {this.prepareResults(matchedResults)}
       </div>
     );
-    
   }
 }
 
